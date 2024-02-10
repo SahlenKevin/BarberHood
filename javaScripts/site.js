44e803c978d71a40e1cb9eba3b2a956370b2359c
Kevin Sahlén <143435191+SahlenKevin@users.noreply.github.com>
2024-02-10 14:44:51 +0100

    addItemToCart

--- javaScripts/site.js
+++ javaScripts/site.js
@@ -6,10 +6,17 @@ console.log(addToCartBtn);
 for (let index = 0; index < addToCartBtn.length; index++) {
     const button = addToCartBtn[index];
     button.addEventListener("click", function(event){
-        const addCartItem = document.createElement("li")
-        addCartItem.classList.add("list-group-item")
-        addCartItem.innerText="Prutt";
-        shoppingcart.appendChild(addCartItem);
-        
+        addItemToCart.call();
     })
+}
+
+function addItemToCart(){
+    const addCartItem = document.createElement("li");
+    const removeButton = document.createElement("btn");
+    removeButton.classList.add("btn", "btn-danger")
+    removeButton.innerText="Remove from cart"
+    addCartItem.classList.add("list-group-item");
+    addCartItem.innerText="Prutt";
+    addCartItem.appendChild(removeButton);
+    shoppingcart.appendChild(addCartItem);
 }
