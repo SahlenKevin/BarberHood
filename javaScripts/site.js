d1d0a62e2b6dd3bc25b0fd237982ce7e67d552e2
Kevin Sahlén <143435191+SahlenKevin@users.noreply.github.com>
2024-02-13 09:50:19 +0100

    Javascript

--- javaScripts/site.js
+++ javaScripts/site.js
@@ -1,15 +1,73 @@
 const shoppingcart = document.getElementById("shopping-cart-list");
 const addToCartBtn = document.getElementsByClassName("btn-success");
-const removeItemFromCartBtn = document.getElementsByClassName("btn-danger");
 const serviceList = document.getElementsByClassName("service-list")[0];
 console.log(addToCartBtn);
 
-for (let index = 0; index < addToCartBtn.length; index++) {
-    let button = addToCartBtn[index];
-    button.addEventListener("click", function(event){
-        addItemToCart.call();
-    })
+const services = [
+    {
+        img: "../images/barber.jpg",
+        Title: "Skägg",
+        Description: "Skäggvård i världsklass",
+        Price: 199
+    },
+    {
+        img: "../images/barber.jpg",
+        Title: "Skägg + hår",
+        Description: "Skäggvård i världsklass + att vi fixar håret på toppen också!",
+        Price: 299
+    }
+]
+
+console.log(services);
+
+displayServices();
+
+function displayServices()
+{
+    for (const service of services) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const img = document.createElement("img");
+        img.classList.add("card-img-top");
+        img.src=service.img;
+        card.appendChild(img);
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+        card.appendChild(cardBody);
+
+        const title = document.createElement("h5");
+        title.classList.add("card-title");
+        title.innerText=service.Title;
+        cardBody.appendChild(title);
+
+        const description = document.createElement("p");
+        description.classList.add("card-text");
+        description.innerText=service.Description;
+        cardBody.appendChild(description);
+
+        const price = document.createElement("p");
+        price.classList.add("card-text");
+        price.innerText=service.Price;
+        cardBody.appendChild(price);
+
+        const addToCartBtn = document.createElement("button");
+        addToCartBtn.classList.add("btn","btn-success");
+        addToCartBtn.innerText="Boka tjänst";
+
+        cardBody.appendChild(addToCartBtn);
+
+        serviceList.appendChild(card);
+    }
 }
+
+// for (let index = 0; index < addToCartBtn.length; index++) {
+//     let button = addToCartBtn[index];
+//     button.addEventListener("click", function(event){
+//         addItemToCart.call();
+//     })
+// }
 
 function addRemoveBtn()
 {
@@ -38,3 +96,11 @@ function addItemToCart(){
     
     addRemoveBtn.call();
 }
+
+const book = {
+  Title: "Titel",
+  Author: "Författare",
+  Year: 1990,
+};
+
+
